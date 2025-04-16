df856c2ab7cebfdd64f4aaf7813d9d699a60d9e4
Yoga Pangestu <yogapangestu16@gmail.com>
2025-04-16 11:34:02 +0700

    update webpage

--- js/javascript.js
+++ js/javascript.js
@@ -11,4 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // Memasukkan text ke header di hero section
+  const insertItaly = document.getElementById("italy-header");
+  if (insertItaly) insertItaly.innerHTML = "Italy";
+  const insertCanada = document.getElementById("canada-header");
+  if (insertCanada) insertCanada.innerHTML = "Canada";
+  const insertAustria = document.getElementById("austria-header");
+  if (insertAustria) insertAustria.innerHTML = "Austria";
+  const insertIndonesia = document.getElementById("indonesia-header");
+  if (insertIndonesia) insertIndonesia.innerHTML = "Indonesia";
+
+
+
+  
 });
